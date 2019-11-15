023ef4511948a3a0d62d7cb28474cfe48a45ab02
heahea0412 <57001978+heahea0412@users.noreply.github.com>
2019-11-15 12:09:19 +0800

    男孩程式動畫控制

--- UnityProject/Assets/scripts/player.cs
+++ UnityProject/Assets/scripts/player.cs
@@ -2,21 +2,67 @@
 
 public class player : MonoBehaviour
 {
+    #region 欄位區域
+    
     //定義欄位 Field
     //欄位類型 欄位名稱(指定 值) 結尾
     //預設 private 私人
     //public 公開
-    [Header("速度"),Range(0f,100f)]
+    [Header("速度"), Range(0f, 100f)]
     public float speed = 3.5f;      //浮點數
-    [Header("跳躍"),Range(100,2000)]
+    [Header("跳躍"), Range(100, 2000)]
     public int jump = 300;          //整數
-    [Header("是否在地板"),Tooltip("用來判定角色是否在地板上")]
+    [Header("是否在地板"), Tooltip("用來判定角色是否在地板上")]
     public bool isGround = false;   //布林值
-   [Header("角色名稱")]
+    [Header("角色名稱")]
     public string _name = "HEAHEA";    //字串
+    [Header("元件")]
+    public Rigidbody2D r2d;
+    public Animator ani;
+    #endregion
 
+    //定義方法
+    //語法
+    //修飾詞 傳回類型 方法名稱(){}
+    //void無傳回
 
+    private void Move()
+    {
+        float h =  Input.GetAxisRaw("Horizontal");   //輸入.取得軸向("水平")左右與AD
+        r2d.AddForce(new Vector2(speed*h, 0));
+        ani.SetBool("跑步開關", h != 0);    //動畫元件.設定布林值
+    }
+    private void Jump()
+    {
+        //如果按下空白鍵 並且 在地板上等於勾選
+        if (Input.GetKeyDown(KeyCode.Space)&& isGround == true)
+        {
+            //在地板上 = 取消
+            isGround = false;
+            //剛體.推力(往上)
+            r2d.AddForce(new Vector2(0,jump));
+        }
+    }
+    private void Dead()
+    {
 
+    }
 
- 
+    //事件:在特定時間點以指定次數執行
+    //更新事件
+    private void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    //碰撞事件:2D物件碰撞時執行一次
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name=="地板")
+        {
+            isGround = true;
+        }
+    }
+        
 }
